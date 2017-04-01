ec6dc237bcd0c5b15f353f4ff83d9aa95657deb4
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-04-01 15:07:14 -0400

    Sets up base for components, adds initial routing for two paths

---
+++ src/app/app.module.js
@@ -0,0 +1,44 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var core_1 = require("@angular/core");
+var platform_browser_1 = require("@angular/platform-browser");
+var forms_1 = require("@angular/forms");
+var router_1 = require("@angular/router");
+var app_component_1 = require("./app.component");
+var crisis_list_component_1 = require("./crisis-list.component");
+var hero_list_component_1 = require("./hero-list.component");
+var not_found_component_1 = require("./not-found.component");
+var appRoutes = [
+    { path: 'crisis-center', component: crisis_list_component_1.CrisisListComponent },
+    { path: 'heroes', component: hero_list_component_1.HeroListComponent },
+    { path: '', redirectTo: '/heroes', pathMatch: 'full' },
+    { path: '**', component: not_found_component_1.PageNotFoundComponent }
+];
+var AppModule = (function () {
+    function AppModule() {
+    }
+    return AppModule;
+}());
+AppModule = __decorate([
+    core_1.NgModule({
+        imports: [
+            platform_browser_1.BrowserModule,
+            forms_1.FormsModule,
+            router_1.RouterModule.forRoot(appRoutes)
+        ],
+        declarations: [
+            app_component_1.AppComponent,
+            hero_list_component_1.HeroListComponent,
+            crisis_list_component_1.CrisisListComponent,
+            not_found_component_1.PageNotFoundComponent
+        ],
+        bootstrap: [app_component_1.AppComponent]
+    })
+], AppModule);
+exports.AppModule = AppModule;
+//# sourceMappingURL=app.module.js.map
